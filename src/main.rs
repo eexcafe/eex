8de2fc4697009baac545de5b98d3525da6e70cc6
王雪 <devel0per@yeah.net>
2022-11-09 03:42:16 +0000

    chore: ci

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// add fn to the end of the file
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_works() {
+        assert_eq!(add(2, 2), 4);
+    }
+}
